f3c5dc5d72551f9aaae705d1be330750366c0dd8
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-28 11:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+     const element = document.getElementById("level");
+        const level = [...element.parentNode.children].indexOf(element);
+alert(`The level of the element is: ${level}`);
